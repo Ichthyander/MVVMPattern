cbb4e3df6f79bb170553ca9cc9e82bdeefd7448e
Ichtyander <di_ignatov@mail.ru>
2022-10-08 21:55:45 +0300

    Добавьте файлы проекта.

---
+++ MVVMPattern/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMPattern
+{
+    //Класс-шаблон для создания команд в архитектуре MVVM
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            //Метод подписывается на событие(связывается с делегатом)
+            add => CommandManager.RequerySuggested += value;
+            //Обратный случай
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object> Execute, Func<object,bool> CanExecute = null)
+        {
+            //Проверка наличия определения действия на команду
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute)); //имя нулевого аргумента Execute
+            canExecute = CanExecute;
+        }
+
+        //поля для записи делегатов
+        private readonly Action<object> execute;
+        private readonly Func<object,bool> canExecute;
+
+        //Проверка canExecute на null с возвратом true (через оператор "??") 
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => execute(parameter);
+    }
+}
---
+++ MVVMPattern/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using MVVMPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMPattern.ViewModels
+{
+    //Реализация INotifyPropertyChanged позволяет связать свойства ViewModel со свойствами View (например, текстовые поля)
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //[CallerMemberName] - атрибут, с помощью которого компилятор может взять название аргумента из вызывающего свойства
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            //? - проверка на null, this - ссылка на ViewModel
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                //[CallerMemberName] позволяет в этом месте оставлять скобки пустыми
+                OnPropertyChanged();
+            }
+        }
+
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                //[CallerMemberName] позволяет в этом месте оставлять скобки пустыми
+                OnPropertyChanged();
+            }
+        }
+
+        //Блок определения команд
+        //Команда
+        public ICommand AddCommand { get; }
+        //метод для подстановки на место Execute из RelayCommand
+        private void OnAddCommandExecute(object p)
+        {
+            //определение длины окружности через ссылку на метод из Models (CircleLength)
+            Length = CircleLength.GetCircleLength(Radius);
+        }
+        //метод для подстановки на место CanExecute из RelayCommand
+        private bool CanAddCommandExecuted(object p)
+        {
+            //место для условий ограничения доступности выполнения команды
+            return true;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
